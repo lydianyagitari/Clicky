e6b401201b88fe9a1caef450147da2e4dc716ed3
unknown <mzinnocents96@gmail.com>
2019-02-25 20:04:54 -0600

    Updates

---
+++ precache-manifest.4c1930197edde52518efeee583d131ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96fadfce2dfe9151f400",
+    "url": "/Clicky/static/js/runtime~main.96fadfce.js"
+  },
+  {
+    "revision": "649264a155e3a7c917b1",
+    "url": "/Clicky/static/js/main.649264a1.chunk.js"
+  },
+  {
+    "revision": "573698287b10209aaf23",
+    "url": "/Clicky/static/js/2.57369828.chunk.js"
+  },
+  {
+    "revision": "649264a155e3a7c917b1",
+    "url": "/Clicky/static/css/main.145f41b0.chunk.css"
+  },
+  {
+    "revision": "e792773847abc64a62d592d887fef70c",
+    "url": "/Clicky/index.html"
+  }
+];
